335688bc4250cee2edb05fafa1d16a392d7821bb
TheUnquiet <zanerebouded@gmail.com>
2023-04-03 11:03:21 +0200

    overall changes

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -1,64 +1,85 @@
 <!doctype html>
 <html lang="en">
-  <head>
-    <title>Title</title>
+
+<head>
+    <title>Admin | Panel</title>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
+    <!-- Link to to little logo  -->
+    <link rel="icon" href="img/logos/logo-top.png" type="x-icon">
+
     <link rel="stylesheet" href="css/form.css">
     <link rel="stylesheet" href="css/main.css">
-  </head>
-  <body>
-    <div class="holder">
-        <div class="form">
-            @if(session('status'))
-                <div class="alert alert-success">
-                    {{ session('status') }}
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    {{-- Chart.js --}}
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.js"></script>
+    <script type="module"  src="js/myChart"></script>
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+
+<body>
+    <div id="holder">
+        <div class="aside text-center">
+            <div class="img-container"><img src="https://media.tenor.com/QT16_24NkpIAAAAM/nicki-minaj-meme.gif"
+                    alt="nicki minaj"></div>
+            <div>
+                <h3 class="text-white p-2">{{ isset(Auth::user()->name) ? Auth::user()->name : 'none' }} | Admin</h3>
+                <ul class="list">
+                    <li class="list-item p-3">
+                        <a href="/welcome">Home</a>
+                    </li>
+                    <li class="list-item p-3 border-top">
+                        <a href="/foods">Foods</a>
+                    </li>
+                    <li class="list-item p-3 border-top">
+                        <a href="/">Intro</a>
+                    </li>
+                    <li class="list-item p-3 border-top">
+                        <a href="{{ url()->previous() }}">Retrun</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class="content">
+            <header>
+                <h1 class="text-left pb-4 pt-3"><b>Dashboard</b></h1>
+            </header>
+
+            <div class="sections">
+                <div class="section">
+                    <i class="fas fa-clipboard"></i>
+                    <br>
+                    Item list. <br>
+                    Name, price, type..
                 </div>
-            @endif
-                <h1 class="head">Add Item to Food table</h1>
-                <div class="inputs">
-                    <form action="/panel" method="post" name="item-form" id="item-form">
-                        @csrf
-                        <div class="inputBox">
-                            <input type="text" name="type" id="type" class="form-control" required placeholder="Type">
-                            <div class="borderLeft"></div>
-                        </div>
-                        <div class="inputBox">
-                            <input type="text" name="name" id="name" class="form-control" required placeholder="Name">
-                            <div class="borderLeft"></div>                   
-                        </div>
-                        <div class="inputBox">
-                            <input type="text" name="price" id="price" class="form-control" required placeholder="Price €">
-                            <div class="borderLeft"></div>
-                        </div>
-    
-                        <div class="buttons">
-                            <input type="submit" class="submit-btn" value="Submit" style="margin-right: 6px;">
-                            <input type="reset" class="submit-btn" value="Reset">
-                            <a href="{{ url()->previous() }}" style="margin-left: auto;">
-                                <input type="button" class="submit-btn" value="Back">
-                            </a>
-                        </div>
-                    </form>
+                <div class="section">
+                    <i class="fa fa-address-card" aria-hidden="true"></i>
+                    <br>
+                    Item list. <br>
+                    Name, price, type..
+                </div>
+                <div class="section">
+                    <i class="fas fa-cookie"></i>
+                    <br>
+                    Item list. <br>
+                    Name, price, type..
+                </div>
+                <div class="section">
+                    <i class="fa fa-server" aria-hidden="true"></i>
+                    <br>
+                    Item list. <br>
+                    Name, price, type..
                 </div>
             </div>
-        </div>    
+            <canvas id="myChart" style="width:100%;max-width:600px"></canvas>
+        </div>
     </div>
-    <!--Waves Container-->
-    <div>
-        <svg class="waves" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
-            viewBox="0 24 150 28" preserveAspectRatio="none" shape-rendering="auto">
-            <defs>
-            <path id="gentle-wave" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z" />                </defs>
-            <g class="parallax">
-                <use xlink:href="#gentle-wave" x="48" y="0" fill="rgba(255,255,255,.7)" />
-                <use xlink:href="#gentle-wave" x="48" y="3" fill="rgba(255,255,255,.5)" />
-                <use xlink:href="#gentle-wave" x="48" y="5" fill="rgba(255,255,255,.3)" />
-            <use xlink:href="#gentle-wave" x="48" y="7" fill="#fff" />                </g>
-        </svg>
-    </div>
-    <!--Waves end-->
-  </body>
+</body>
+
 </html>
